44cbecd550b28a7502a0beb25fb35a7bd1335f38
M1kael-Rocha <mikaelbernardes@gmail.com>
2025-10-02 17:17:10 -0300

    Mudanças/Alterações sendo testadas

--- app/lib/credentials-techniques.js
+++ app/lib/credentials-techniques.js
@@ -1,9 +1,15 @@
 'use server';
 
-import { readDb, writeDb } from './connection-db';
+import { readDb } from '@/app/lib/connection-db';
 
 const file = 'techniques-db.json';
 
 export async function getAllTechniques() {
   return await readDb(file);
+}
+
+export async function getTechniqueById(id) {
+  const techniques = await readDb(file);
+  const technique = techniques.find(t => t.id === id);
+  return technique;
 }
